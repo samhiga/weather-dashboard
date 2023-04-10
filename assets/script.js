d5cd70f378cb742d1d565c0152a940401806990b
Sam Higa <samhiga@Sams-MacBook-Pro.local>
2023-04-09 19:42:48 -0700

    added weather for current day

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,26 @@
 var cityWeather = document.getElementById("city-weather")
 
 cityWeather.addEventListener("submit", function(event){
-event.preventDefault()
+    event.preventDefault()
 var cityInput = document.getElementById("city-input").value
 console.log(cityInput)
-var weatherForcast = "https://api.openweathermap.org/data/2.5/forecast?q="+cityInput+"&appid=ad7da4caf04d6e1954be62c0bd33ba95"
+var weatherForcast = "https://api.openweathermap.org/data/2.5/forecast?q="+cityInput+"&appid=ad7da4caf04d6e1954be62c0bd33ba95&units=imperial"
+
 fetch(weatherForcast).then(function(response){
 return response.json()
 .then(function(data){
 console.log(data)
+
+var day = data.list[0]
+
+var humidity = document.getElementById("humidity")
+
+var wind = document.getElementById("wind")
+
+var temperature = document.getElementById("temperature")
+wind.textContent = "Wind "+ day.wind.speed + " MPH"
+humidity.textContent = "Humidity " + day.main.humidity + "%"
+temperature.textContent = "Temp "+ day.main.temp + "°F"
 })
 })
 })
